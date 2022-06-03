4e80b3d86bd224c2d5d676bef5ec6664bf46a804
OkothPius <orukopius8@gmail.com>
2022-06-03 12:57:44 +0300

    feat: update terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+locals { 
+
+    location = "uksouth" 
+
+    prefix = "gitopsdemo" 
+
+  } 
+
 terraform { 
 
   required_providers { 
@@ -21,3 +29,67 @@ provider "azurerm" {
   features {} 
 
 } 
+
+resource "azurerm_resource_group" "main" {
+
+  name     = "${local.prefix}-rg"
+
+  location = local.location
+
+  }
+
+
+
+resource "azurerm_storage_account" "main" {
+
+  name                     = "${local.prefix}storageacct"
+
+  resource_group_name      = azurerm_resource_group.main.name
+
+  location                 = azurerm_resource_group.main.location
+
+  account_tier             = "Standard"
+
+  account_replication_type = "LRS"
+
+}
+
+
+
+resource "azurerm_service_plan" "main" {
+
+  name                = "${local.prefix}-asp"
+
+  resource_group_name = azurerm_resource_group.main.name
+
+  location            = azurerm_resource_group.main.location
+
+  os_type            = "Linux"
+
+  sku_name            = "Y1"
+
+}
+
+
+
+resource "azurerm_linux_function_app" "main" {
+
+  name                       = "${local.prefix}-function"
+
+  resource_group_name        = azurerm_resource_group.main.name
+
+  location                   = azurerm_resource_group.main.location
+
+
+
+  service_plan_id            = azurerm_service_plan.main.id
+
+  storage_account_name       = azurerm_storage_account.main.name
+
+  storage_account_access_key = azurerm_storage_account.main.primary_access_key
+
+
+
+  site_config {}
+
+}
